31f54d03a19598fc594d210a8a99d7675be28074
Tu Duyen Nguyen <125369405+tuduyen-nguyen@users.noreply.github.com>
2025-04-28 23:17:44 +0200

    Delete +estim_NK directory

--- +estim_NK/+sparse/+block/dynamic_8.m
+++
@@ -1,3 +0,0 @@
-function [y, T] = dynamic_8(y, x, params, steady_state, sparse_rowval, sparse_colval, sparse_colptr, T)
-  y(49)=params(30)*y(69);
-end
--- +estim_NK/+sparse/+block/static_10.m
+++
@@ -1,3 +0,0 @@
-function [y, T] = static_10(y, x, params, sparse_rowval, sparse_colval, sparse_colptr, T)
-  y(14)=params(30)*y(34);
-end

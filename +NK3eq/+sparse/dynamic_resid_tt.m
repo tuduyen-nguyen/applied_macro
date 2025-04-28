5da35f343be6fc514633f7e475a314e0bad82152
Tu Duyen Nguyen <125369405+tuduyen-nguyen@users.noreply.github.com>
2025-04-28 23:17:28 +0200

    Delete +NK3eq directory

--- +NK3eq/+sparse/dynamic_resid_tt.m
+++
@@ -1,9 +0,0 @@
-function [T_order, T] = dynamic_resid_tt(y, x, params, steady_state, T_order, T)
-if T_order >= 0
-    return
-end
-T_order = 0;
-if size(T, 1) < 0
-    T = [T; NaN(0 - size(T, 1), 1)];
-end
-end
--- +NK3eq/+sparse/static_resid_tt.m
+++
@@ -1,9 +0,0 @@
-function [T_order, T] = static_resid_tt(y, x, params, T_order, T)
-if T_order >= 0
-    return
-end
-T_order = 0;
-if size(T, 1) < 0
-    T = [T; NaN(0 - size(T, 1), 1)];
-end
-end
